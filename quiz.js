512278749cc698a944893f737049b0bf3548ecca
harshparab <harsh_parab@outlook.com>
2021-09-22 18:27:04 +0530

    added questions

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form")
 const submitAnswerBtn = document.querySelector("#submit-answer-btn")
 const output = document.querySelector("#output")
 
-const correctAnswers = ["90°", "right angled"]
+const correctAnswers = ["90°", "right angled","15 cm","35 sq.cm","scalene"]
 
 function calculateScore() {
     let score = 0;
